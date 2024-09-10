438d40a449241659cbe6d7546fc51c5b51771c8b
Gloria Jimena <gloriajimenez010@icloud.com>
2024-09-10 11:27:53 +0200

    primerCambio

---
+++ prueba2/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.Write("Congtratulations");
+Console.Write("  ");
+Console.Write("you wrote your first lines of code");
+
