776a8216d601373a5d73737080ac7e8c0ae89854
Gloria Jimena <gloriajimenez010@icloud.com>
2024-09-11 11:36:31 +0200

    declaracionVariables

--- prueba2/Program.cs
+++ prueba2/Program.cs
@@ -1,6 +1,44 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.Write("Congtratulations");
-Console.Write("  ");
-Console.Write("you wrote your first lines of code");
+
+/*Console.WriteLine("Hello, World!");
+Console.WriteLine("Congtratulations");
+Console.WriteLine("  ");
+Console.WriteLine("you wrote your first lines of code");
+
+Console.WriteLine("Es la primera linea");
+Console.WriteLine("Es la segunda linea");
+
+Console.Write('b');
+
+Console.WriteLine("123");
+Console.WriteLine(123);*/
+
+//CREACION DE LA PRIMERA VARIABLE
+
+string firstName;
+//firstName= "";
+//Console.WriteLine(firstName);
+firstName="Gloria";
+Console.WriteLine(firstName);
+firstName="Bob";
+Console.WriteLine(firstName);
+firstName="Isabella";
+Console.WriteLine(firstName);
+firstName="Yasmin";
+Console.WriteLine(firstName);
+
+var message="hello";
+
+//1EJERCICIO
+var name="Bob";
+var number=3;
+var number2 =32.1;
+Console.Write("Hello, mi name is " +  name  +  " i have " +  number  +  " now " +   number2);
+//OTRA MANERA DE HACERLO
+string name2= "Bob";
+int num1=3;
+decimal num2= 32.4m;
+
+
+
 
