c60a16dcd9e74fb3bb65b2b1bc2f7f9bdb447ff3
Gloria Jimena <gloriajimenez010@icloud.com>
2024-09-12 12:57:29 +0200

    IncrementoDecremento

--- prueba2/Program.cs
+++ prueba2/Program.cs
@@ -1,46 +1,38 @@
-﻿//SUMAS CON CONVERSION IMPLICITA DE DATOS
-
-using System.Data;
-
-int firstNUmber = 12;
-int secondNumber = 7;
-Console.WriteLine(firstNUmber + secondNumber);
-
-string nombre ="Bob";
-int widgetsSold= 7;
-Console.WriteLine (nombre + " sold " + widgetsSold + " widgets ");
-///<concatena el +7, es decir, quedaria igual a 77>
-Console.WriteLine(nombre + " sold " + widgetsSold + 7 + " widgets ");
-/// <con el parentisis se convierte en otra operacion lo que daria 7+7+14> 
-Console.WriteLine(nombre + " sold " + (widgetsSold + 7) + " widgets ");
-
-//EJERCICIOS
-//SUMA RESTA MULTIPLICACION Y DIVISION
- int num1 = 10;
- int num2 = 6;
-int resto= num1 % num2;
- Console.WriteLine(num1 + num2);
- Console.WriteLine(num1 - num2);
- Console.WriteLine(num1 * num2);
- Console.WriteLine(num1 / num2);
- //division de enteros sacando el resultado de 
- Console.WriteLine((decimal)num1 / (decimal)num2);
- //resto de la division
- Console.WriteLine(resto);
-
-///DIVISION CON DATOS DECIMALES
- 
-double number = 10.2;
-double number2 =  2.5;
-Console.WriteLine( number / number2);
-
-/// ANAIDIR LA DIVISION UTILIZANDO DATOS DECIMALES
-decimal de = (decimal) firstNUmber / (decimal) secondNumber;
-
-///SACAR EL RESTO DE UNA DIVISION
-///int dividiendo= 20;
-///int divisor= 6;
-//int resto = dividiendo % divisor;
-///Console.WriteLine(dividiendo / divisor % resto);
+﻿///INCREMENTO Y DECREMENTO
 
 
+//int value = 0;    //value is now 0
+//value = value + 5; //value is now 5
+//value +=5;          //value is now 10
+
+
+//int value = 0;    //value is now 0
+//value = value + 1; //value is now 1
+//value ++;          //value is now 2
+
+/// -----------------------------------------------------------///
+
+
+///AUMENTAR Y DISMINUIR UN VALOR
+ int value = 1;
+ value = value + 1;
+ Console.WriteLine("Primer incremento: " + value);
+value += 1;
+ Console.WriteLine("Primer incremento: " + value);
+value++;
+ Console.WriteLine("Primer incremento: " + value);
+ value = -1;
+ Console.WriteLine("Primer incremento: " + value);
+ value -=1;
+ Console.WriteLine("Primer incremento: " + value);
+value--;
+ Console.WriteLine("Primer incremento: " + value);
+
+/// -----------------------------------------------------------///
+
+//CALCULO GRADOS FAHRENHEIT A CELSIUS
+var FAHRENHEIT = 94;
+var CELSIUS = (FAHRENHEIT - 32m) * (5m / 9m);
+Console.WriteLine("La temperatura es " + Math.Round(CELSIUS,1) + "CELSIUS");
+Console.WriteLine("La temperatura es " + CELSIUS.ToString ("##.#") + "CELSIUS");
+Console.WriteLine("La temperatura es " + decimal.Round(CELSIUS,1) + "CELSIUS");
