b771fe2004cf7a71f9d550d315045cd28c67c8d4
Gloria Jimena <gloriajimenez010@icloud.com>
2024-09-16 11:21:48 +0200

    condicional IF 2

--- prueba2/Program.cs
+++ prueba2/Program.cs
@@ -1,28 +1,52 @@
-﻿using System.Net;
+﻿Random random = new Random();
 
-Random random = new Random();
+var discountPercentage = 10;
+var continua =  true;
 
-var discountPercentage = 0;
-for (int i = 0; i == 1; i ++);
-while(true){
-var daysUntilExpiration = random.Next(12);
+while (continua)
+{
+    var daysUntilExpiration = random.Next(12);
 
-Console.Clear();
-Console.WriteLine("Numero de dias que expire: " + daysUntilExpiration);
-if (daysUntilExpiration > 5){
-    Console.WriteLine("Tu suscripcion esta a punto de expirar")
-}
-else if(daysUntilExpiration <= 5 && daysUntilExpiration > 1){
-    Console.WriteLine("Your subscription will in " + daysUntilExpiration + " days. Renew now and save 10%");
-}
-else if (daysUntilExpiration == 1 ){
-    Console.WriteLine("Tu suscripcion expira hoy. Renueva y ahorrate un" + discountPercentage + "%");
+
+    Console.Clear();
+
+    Console.WriteLine("Numero de dias para que expire: " + daysUntilExpiration);
+    if (daysUntilExpiration > 5)
+    {
+        Console.WriteLine("Tu suscripción está a punto de expirar. Renuevela ahora!");
+    }
+    else if (daysUntilExpiration <= 5 && daysUntilExpiration > 1)
+    {
+        Console.WriteLine("Tu suscripción está expira en " + daysUntilExpiration + " dias.");
+    }
+    else if (daysUntilExpiration == 1)
+    {
+        Console.WriteLine("Tu suscripción expira hoy \n Renueva y ahorrate un " + discountPercentage + "%");
+    }
+    else
+    {
+        Console.WriteLine("Tu suscripción ha expirado");
+    }
+
+
+    switch (daysUntilExpiration)
+    {
+        case int n when (n > 5):
+            Console.WriteLine("Tu suscripción está a punto de expirar. Renuevela ahora!");
+            break;
+        case int n when (n <= 5 && n > 3):
+            Console.WriteLine("Tu suscripción está expira en " + daysUntilExpiration + " dias.");
+            break;
+        case int n when (n == 1):
+            Console.WriteLine("Tu suscripción expira hoy \n Renueva y ahorrate un " + discountPercentage + "%");
+            break;
+        default:
+            Console.WriteLine("Tu suscripción ha expirado");
+            break;
+    }
+
+    var tecla = Console.ReadKey();
+    if(tecla.KeyChar == 'x')
+    {continua = false;}
 
 }
-else{
-Console.WriteLine("Tu suscripcion ha expirado");
-}
-
-}
-var tecla = Console.ReadKey();
-if(tecla.KeyChar == 'x');
