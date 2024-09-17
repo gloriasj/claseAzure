f8f889f1620752206e6cbc5517f628824e8291b9
Gloria Jimena <gloriajimenez010@icloud.com>
2024-09-17 13:13:39 +0200

    codigo cara o cruz

--- prueba2/Program.cs
+++ prueba2/Program.cs
@@ -1,59 +1,8 @@
-﻿using System;
+﻿int saleAmount = 1001;
+// int discount = saleAmount > 1000 ? 100 : 50;
 
-Console.Clear();
+Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
 
-// Crear un array de nombres
-string[] alumnos = { "Sophia", "Andrew", "Emma", "Logan","Gloria" };
-
-// Crear un array de arrays de enteros
-//Inicializar directamente
-int[][] notas = new int[][]
-{
- new int[] { 90, 86, 87, 98, 100 },
- new int[] { 92, 89, 81, 96, 90 },
- new int[] { 90, 85, 87, 98, 68 },
- new int[] { 90, 95, 87, 88, 96 },
- new int[] { 90, 97, 67, 70, 96 },
-  new int[] { 90, 97, 67, 70, 96 },
-
-};
-if(alumnos.Length != notas.Length){
-    Console.WriteLine("El numero de alumnos no coincide con el numero de notas");
-}
-else{
-
-}
-var indiceDeAlumno = 0;
-decimal sumaDeValores = 0;
-// Mostrar los nombres y sus valores asociados
-foreach (var alumno in alumnos)
-{
-    sumaDeValores = notas[indiceDeAlumno].Sum();
-
-    var media = sumaDeValores / (notas[0].Length );
-    indiceDeAlumno++;
-    sumaDeValores = 0;
-
-
-    var notaEnLetra = "F";
-    if (media <= 59) { notaEnLetra = "F"; }
-    else if (media <= 62) { notaEnLetra = "D-"; }
-    else if (media <= 66) { notaEnLetra = "D"; }
-    else if (media <= 69) { notaEnLetra = "D+"; }
-    else if (media <= 72) { notaEnLetra = "C-"; }
-    else if (media <= 76) { notaEnLetra = "C"; }
-    else if (media <= 79) { notaEnLetra = "C+"; }
-    else if (media <= 82) { notaEnLetra = "B-"; }
-    else if (media <= 86) { notaEnLetra = "B"; }
-    else if (media <= 89) { notaEnLetra = "B+"; }
-    else if (media <= 92) { notaEnLetra = "A-"; }
-    else if (media <= 96) { notaEnLetra = "A"; }
-    else if (media <= 100) { notaEnLetra = "A+"; }
-    else { notaEnLetra = "Out Range"; }
-
-    Console.WriteLine($"Nombre: {alumno} \t Valores: {media} \t {notaEnLetra}");
-
-}
-
-Console.WriteLine("Press the Enter key to continue");
-Console.ReadLine();
+Random coin = new Random();
+//Console.WriteLine($"coin: {((coin.Next(0,2) == 0  ? "heads" : "tails"))}
+while(true) {Console.WriteLine(new Random().Next(0,2) < 1 ? "cara" : "cruz"); Console.ReadKey();}
